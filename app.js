db0ad6c862bb28f716dc11d79701a5d8e7cdfe67
farid <valkanid12@gmail.com>
2024-10-26 21:38:08 +0700

    middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./config/db");
 const allRoutes = require("./routes");
+const cors = require("cors");
 
 db.then(() => {
   console.log("koneksi database berhasil");
@@ -12,6 +13,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(allRoutes);
 
