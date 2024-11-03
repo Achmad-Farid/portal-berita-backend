e873d4ad71f22510ff1bd4208a4ab460af1c3265
farid <valkanid12@gmail.com>
2024-11-03 22:16:34 +0700

    domain

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use(
       secure: false, // Ubah ke true jika menggunakan HTTPS
       maxAge: 1000 * 60 * 60 * 24, // 1 hari
       sameSite: "none",
+      domain: "https://portal-berita-wh1t.vercel.app",
     },
   })
 );
