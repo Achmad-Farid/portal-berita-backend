4a8163532ec3d5fc69ab3e699fcc73f21c951ca8
farid <valkanid12@gmail.com>
2024-11-03 20:15:47 +0700

    test server

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const route = express.Router();
 
 const userRoute = require("./authRoute");
 
+route.get("/", (req, res) => {
+  res.status(200).send("Server is up and running!");
+});
+
 route.use("/auth", userRoute);
 
 module.exports = route;
